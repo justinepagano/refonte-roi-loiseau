fe5bfe24e9bceb542aec4c05f2721bbcf959f611
justinepagano <justine.pagano@etu.uca.fr>
2022-09-17 18:09:30 +0200

    Update script-justine.js

---
+++ js/script-justine.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".liste").click(function(){
+        const value = $(this).attr("data-filter");
+        if (value == "all"){
+            $(".itemBox").show("1000")
+        }
+        else{
+            $(".itemBox").not("."+value).hide("1000");
+            $(".itemBox").filter("."+value).show("1000");
+        }
+    })
+    $(".liste").click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+    })
+})
